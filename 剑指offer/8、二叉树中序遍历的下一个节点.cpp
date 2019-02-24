d72cd75979c293686a10b4438cc7687e72cee6a4
David <1165902905@qq.com>
2019-02-24 19:48:08 +0800

    剑指offer-问题7-day47

---
+++ 剑指offer/8、二叉树中序遍历的下一个节点.cpp
@@ -0,0 +1,34 @@
+/*
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+        
+    }
+};
+*/
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        if(pNode == nullptr)
+            return nullptr;
+
+        if(pNode->right != nullptr)  //如果有右子树，则找右子树的最左节点
+        {
+            TreeLinkNode* pRight = pNode->right;
+            while(pRight->left)
+                pRight = pRight->left;
+            return pRight;
+        }
+        while(pNode->next != nullptr)  //没右子树，则找第一个当前节点是父节点左孩子的节点
+        {
+            if(pNode->next->left == pNode)
+                return pNode->next;
+            pNode = pNode->next;
+        }
+        return nullptr;  //退到了根节点仍没找到，则返回null
+    }
+};
